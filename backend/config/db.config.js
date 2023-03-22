5206d224532523d8cbe4b87f8cbe3e19427e7c1c
Pera Trusnik <40424206+peratrusnik@users.noreply.github.com>
2023-03-22 12:00:48 +0100

    Update db.config.js

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,4 +1,4 @@
-const MONGO_DB_PASSWORD = 'shop2023'
+const MONGO_DB_PASSWORD = ''
 const MONGO_DB_URL = `mongodb+srv://onlineshop:${MONGO_DB_PASSWORD}@cluster0.3l62v.mongodb.net/?retryWrites=true&w=majority`
 
 module.exports = MONGO_DB_URL
